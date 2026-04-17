3de46ea00642656d4cfdc6acf2a442173743276b
Beunard <Beunard.le@gmail.com>
2026-04-17 18:46:15 -0400

    nightmode created and slight ui adjustments

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -28,7 +28,7 @@ const SidebarLink = ({
         <Link href={href}>
             <div className={` cursor-pointer flex items-center ${
                 isCollapsed ? "justify-center py-4" : 'justify-start px-8 py-4'} hover:text-blue500 hover:bg-blue-100 gap-3 transition-colors ${
-                    isActive ? "text-white bg-blue-200" : ""}`}>   
+                    isActive ? "text-white bg-blue-500" : ""}`}>   
                 <Icon className="w-6 h-6 !text-gray-700" />
 
                 <span className={`${isCollapsed ? "hidden" : "block"} font-medium text-gray-700`}> 
